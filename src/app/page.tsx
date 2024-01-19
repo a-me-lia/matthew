31c7a9b381ad8cb15bfb9bf92bd469726078f07a
Matthew Guo (HP) <matthewguo.x86@gmail.com>
2024-01-19 09:08:46 -0500

    add back ypasbi image
    Signed-off-by: Matthew Guo (HP) <matthewguo.x86@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client"
 
+import Image from 'next/image'
+
 import SignUpButton from './components/signupbutton'
+
 
 
 export default function Home() {
 
+
   return (
 <main className='h-[calc(100dvh)]'>
+    <div className='w-full h-full absolute z-0'>
+      <Image src='/yoasobi.webp' alt='yoasobi' fill className='object-cover'></Image>
+    </div>
+
+
+
     <div className='relative flex flex-col items-center justify-center z-50 h-full space-y-8'>
-      <h1 className='font-mono text-2xl font-medium text-white text-shadow '>Coming Soon</h1>
+      <h1 className='font-mono md:text-7xl text-2xl font-medium text-white  text-shadow '>Coming Soon</h1>
       <SignUpButton text='Sign up for updates!' ></SignUpButton>
     </div>
 </main>
