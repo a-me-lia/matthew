582fad54f0ba4ee9623a8885ee98dc5aecbad882
emelia paisley <emelia@emelia.pl>
2024-02-09 03:18:37 -0500

    sdgwertg
    Signed-off-by: emelia paisley <emelia@emelia.pl>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
 <main className='h-[calc(100dvh)]'>
     <div className='w-full h-full absolute z-0'>
-      <Image src='/yoasobi.webp' alt='yoasobi' fill className='object-cover'></Image>
+      <Image src='/c.png' alt='PYM-A1' fill className='object-cover'></Image>
     </div>
 
     <div className='relative flex flex-col items-center justify-center z-50 h-full space-y-8'>
